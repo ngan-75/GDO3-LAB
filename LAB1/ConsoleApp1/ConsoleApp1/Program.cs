41365ed64a61fa8ff193ab78fcdf9da32cd5246f
ngan-75 <nguyenngan684148@gmail.com>
2025-06-10 10:53:44 +0700

    tinhtonghtt

---
+++ LAB1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static int hieu(int a,int b)
+        {
+            return a - b;
+        }
+        static int tong(int a,int b)
+        {
+            return a + b;
+        }
+        static float tich (int a,int b)
+        {
+            return a / b;
+        }
+        static int thuong(int a,int b)
+        {
+            return a * b;
+        }
+        
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("nhap so a");
+            a = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("nhap so b");
+            b = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("tong cua a va b:{0}", tong(a, b));
+            Console.WriteLine("hieu cua a va b:{0}", hieu(a, b));
+            Console.WriteLine("tich cua a va b:{0}", tich(a, b));
+            Console.WriteLine("thuong cua a va b:{0}", thuong(a, b));
+        }
+
+    }
+}
