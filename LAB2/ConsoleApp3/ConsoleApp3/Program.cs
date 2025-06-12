1d98322668dd5a3e682a5be85fd4540c27a06d0a
ngan-75 <nguyenngan684148@gmail.com>
2025-06-12 10:22:26 +0700

    key

---
+++ LAB2/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht01 = new Hashtable();
+            ht01.Add("a", 1);
+            ht01.Add("b", 2);
+            ht01.Add("c", 3);
+            ht01.Add("d", 4);
+            ht01.Add("e", 5);
+            ht01.Add(1, "c");
+            //Hashtable.Remove: Xoa theo Key
+            //ht01.Remove(1);
+            bool hasKey = ht01.ContainsKey("c");
+            if (hasKey) ht01.Remove("c");
+            hasKey = ht01.ContainsKey("f");
+            if (hasKey) ht01.Remove("f");
+
+            bool hasValue = ht01.ContainsValue(3);
+            hasValue = ht01.ContainsValue(6);
+
+
+            foreach (DictionaryEntry item in ht01)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- LAB3/ConsoleApp1/Program.cs
+++ LAB3/ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace ConsoleApp1
 {
@@ -10,6 +11,42 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            ArrayList list01= new ArrayList();
+            list01.Add(1);
+            list01.Add(2);
+            list01.Add(3);
+            list01.Add(4);
+            list01.Add(5);
+
+            for (int i=0;i<list01.Count;i++)
+            {
+                Console.WriteLine($"Item {i}: {list01[i]}");
+            }
+            list01.RemoveAt(3);
+            list01.Insert(4,10);
+            list01.Insert(2, 8);
+            Console.WriteLine($"List01 Count:{list01.Count}");
+            ArrayList list02 = new ArrayList();
+            list02.Add("a1");
+            list02.Add("b1");
+            list02.Add("c1");
+            list02.Add("d1");
+            list02[2] = "c2";
+            list02.Add(100);
+            list02.Add(3.14f);
+            list01.InsertRange(4, list02);
+            list01.InsertRange(6, list02);
+            list02.Remove("c1");
+            list02.Remove("c2");
+            list02.Clear();
+
+            list01.RemoveRange(6,5);
+            for (int i = 0; i < list02.Count; i++)
+            {
+                Console.WriteLine($"List01 Count:{list02.Count}");
+            }
+            Console.ReadLine();
+
         }
     }
 }
