fb9f8594e06d8ff5579ee4dfba606aad48543e9f
ngan-75 <nguyenngan684148@gmail.com>
2025-06-17 09:12:03 +0700

    hashtable

--- LAB4/ConsoleApp1/Program.cs
+++ LAB4/ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp4
+namespace ConsoleApp1
 {
     internal class Program
     {
--- LAB4/ConsoleApp1/Properties/AssemblyInfo.cs
+++ LAB4/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp4")]
+[assembly: AssemblyTitle("ConsoleApp1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp4")]
+[assembly: AssemblyProduct("ConsoleApp1")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ccfa72a1-5fd8-4425-b35a-9a50c81d89e1")]
+[assembly: Guid("b7cbbd31-f95a-484f-9731-159aabcd286f")]
 
 // Version information for an assembly consists of the following four values:
 //
